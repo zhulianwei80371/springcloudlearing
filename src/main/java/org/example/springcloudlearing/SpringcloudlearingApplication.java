709b6dbbc8d373c0109f115ab859b18194fa0c61
zhulianwei80371 <632229677@qq.com>
2024-08-26 18:09:03 +0800

    字典  测试事务

--- src/main/java/org/example/springcloudlearing/SpringcloudlearingApplication.java
+++ src/main/java/org/example/springcloudlearing/SpringcloudlearingApplication.java
@@ -6,18 +6,22 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.jdbc.support.JdbcTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
 @MapperScan(basePackages ={"com.gitee.sunchenbin.mybatis.actable.dao.*","org.example.springcloudlearing.mapper"})
+//@MapperScan(basePackages ={"org.example.springcloudlearing.mapper"})
 @ComponentScan(basePackages = {"com.gitee.sunchenbin.mybatis.actable.manager.*","org.example.springcloudlearing.*"})
+@EnableTransactionManagement
 public class SpringcloudlearingApplication {
 
     public static void main(String[] args) {
         ConfigurableApplicationContext run =  SpringApplication.run(SpringcloudlearingApplication.class, args);
-        // 容器中获取actable的核心处理类
-        StartUpHandler bean =run.getBean(StartUpHandler.class, args);
-        // 手动执行actable的建表方法
-        bean.startHandler();
+//        // 容器中获取actable的核心处理类
+//        StartUpHandler bean =run.getBean(StartUpHandler.class, args);
+//        // 手动执行actable的建表方法
+//        bean.startHandler();
     }
 
 }
